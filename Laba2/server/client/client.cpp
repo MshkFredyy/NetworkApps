e3debffc1b816a0da966df9f21a36fde4a16a61e
MshkFredyy <147163295+MshkFredyy@users.noreply.github.com>
2025-11-16 17:11:39 +0400

    Add files via upload

---
+++ Laba2/server/client/client.cpp
@@ -0,0 +1,54 @@
+﻿#define WIN32_LEAN_AND_MEAN
+#include <iostream>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    WSADATA wsaData;
+    int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (iResult != 0)
+    {
+        std::cerr << "Ошибка WSAStartup: " << iResult << "\n";
+        return 1;
+    }
+
+    SOCKET ConnectSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (ConnectSocket == INVALID_SOCKET)
+    {
+        std::cerr << "Ошибка создания сокета\n";
+        WSACleanup();
+        return 1;
+    }
+
+    sockaddr_in clientService;
+    clientService.sin_family = AF_INET;
+    clientService.sin_port = htons(27015);
+    inet_pton(AF_INET, "127.0.0.1", &clientService.sin_addr);
+
+    iResult = connect(ConnectSocket, (SOCKADDR*)&clientService, sizeof(clientService));
+    if (iResult == SOCKET_ERROR) {
+        std::cerr << "Ошибка соединения\n";
+        closesocket(ConnectSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    const char* sendbuf = "Привет от клиента сервера!";
+    send(ConnectSocket, sendbuf, (int)strlen(sendbuf), 0);
+
+    char recvbuf[512];
+    iResult = recv(ConnectSocket, recvbuf, 512, 0);
+    if (iResult > 0) {
+        recvbuf[iResult] = '\0';
+        std::cout << "Ответ сервера: " << recvbuf << "\n";
+    }
+
+    closesocket(ConnectSocket);
+    WSACleanup();
+    return 0;
+}
+
---
+++ Laba2/server/server/server.cpp
@@ -0,0 +1,77 @@
+﻿#define WIN32_LEAN_AND_MEAN
+#include <iostream>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    WSADATA wsaData;
+    int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (iResult != 0)
+    {
+        std::cerr << "WSAStartup ошибка: " << iResult << "\n";
+        return 1;
+    }
+
+    SOCKET ListenSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (ListenSocket == INVALID_SOCKET)
+    {
+        std::cerr << "Ошибка создания сокета\n";
+        WSACleanup();
+        return 1;
+    }
+
+    sockaddr_in service;
+    service.sin_family = AF_INET;
+    service.sin_addr.s_addr = INADDR_ANY;
+    service.sin_port = htons(27015);
+
+    if (bind(ListenSocket, (SOCKADDR*)&service, sizeof(service)) == SOCKET_ERROR)
+    {
+        std::cerr << "Ошибка привязки\n";
+        closesocket(ListenSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    if (listen(ListenSocket, SOMAXCONN) == SOCKET_ERROR)
+    {
+        std::cerr << "Ошибка прослушивания\n";
+        closesocket(ListenSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    std::cout << "Ожидание соединения с клиентом...\n";
+
+    SOCKET ClientSocket = accept(ListenSocket, NULL, NULL);
+    if (ClientSocket == INVALID_SOCKET)
+    {
+        std::cerr << "Ошибка присоединения\n";
+        closesocket(ListenSocket);
+        WSACleanup();
+        return 1;
+    }
+    closesocket(ListenSocket);
+
+    char recvbuf[512];
+    int recvbuflen = 512;
+
+    iResult = recv(ClientSocket, recvbuf, recvbuflen, 0);
+    if (iResult > 0)
+    {
+        recvbuf[iResult] = '\0';
+        std::cout << "Получено: " << recvbuf << "\n";
+
+        const char* sendbuf = "Сообщение получено: ";
+        send(ClientSocket, sendbuf, (int)strlen(sendbuf), 0);
+    }
+
+    closesocket(ClientSocket);
+    WSACleanup();
+    return 0;
+}
+
